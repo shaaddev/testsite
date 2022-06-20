eb395061bae822aaeed925ccff238b8c43b7ca83
Rashaad Lee Hue <rashaadleehue@qwe.local>
2022-06-20 04:36:08 -0400

    --

--- action.php
+++ action.php
@@ -7,22 +7,11 @@
     $company = $_POST['company'];
     $comment = $_POST['comment'];
 
-    $conn = new mysqli('localhost', 'root', '', 'Random');
-
-    // chech connection
-    if ($conn->connect_error) {
-        die ('Connection Failed!');
-    } else {
-        $stmt = $conn->prepare("insert into Randomtable (Fullname, Email, Company, Contact) 
-            VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $name, $email, $company, $comment);
-        $stmt->execute();
-        echo "Subscription Successful!";
-
-        $stmt->close();
-        $conn->close();
-    }
-
-   
+    // initalize table
+    $sql = 'INSERT INTO Contact (Fname, Email, Company, Message) VALUES (:fname, :email, :company, :message)';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['fname' => $name, 'email' => $email, 'company' => $company, 'message' => $comment]);
+    
+    echo "post added";
 
 ?>
--- connect.php
+++ connect.php
@@ -1,6 +1,18 @@
 
 <?php
 
-    
+    $host = 'localhost';
+    $dbname = 'Random';
+    $user = 'root';
+    $password = '';
 
+    // DSN
+    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+    # output = 'mysql:host=localhost; dbname=Random
+
+    // PDO connection
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTIONS);
+
+    echo "Connected !";
 ?>
